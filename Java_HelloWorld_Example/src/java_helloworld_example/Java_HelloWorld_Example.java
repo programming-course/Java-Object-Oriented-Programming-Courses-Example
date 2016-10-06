63d726570d8ac4f04716c2dc3bf5a12d9c60eae2
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-10-06 20:01:03 +0300

    master

---
+++ Java_HelloWorld_Example/src/java_helloworld_example/Java_HelloWorld_Example.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_helloworld_example;
+
+/**
+ *
+ * @author root
+ */
+public class Java_HelloWorld_Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
