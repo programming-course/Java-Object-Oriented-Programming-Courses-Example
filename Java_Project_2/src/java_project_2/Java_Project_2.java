1fcce704e986ef9cf98dff86ce4d0e6bff1d3998
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-10-11 10:37:37 +0300

    master

---
+++ Java_Project_2/src/java_project_2/Java_Project_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_2;
+
+/**
+ *
+ * @author labstudent
+ */
+public class Java_Project_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        System.out.println("Hi");
+        System.out.print("Java");
+        System.out.print("Project");
+        System.out.println("How are " + "you ?" + 99);
+        System.out.println(7+5);
+        System.out.println("7"+"5"); 
+    }
+    
+}
