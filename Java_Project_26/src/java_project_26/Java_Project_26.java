5977dbfe586f14830d3fa35e338a43fa0d70ce26
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-01 14:20:11 +0300

    master

--- Java_Project_26/src/java_project_26/Java_Project_26.java
+++ Java_Project_26/src/java_project_26/Java_Project_26.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package java_project_26;
-
+import java.util.Scanner;
 /**
  *
  * @author ismailtasdelen
@@ -16,6 +16,22 @@ public class Java_Project_26 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //
+        // Örnek :
+        //
+        double avm, indirim=0, odeme=0;
+        Scanner tara = new Scanner(System.in);
+        System.out.println("Alışveriş miktarınızı giriniz : ");
+        avm = tara.nextDouble();
+        if (avm <= 1000)
+            indirim = avm * 5 / 100;
+        else if (avm >= 1000 & avm < 10000)
+            indirim = avm * 8 /100;
+        else if (avm >=10000)
+            indirim = avm * 8.5 / 100;
+            
+        odeme = avm - indirim;
+        System.out.println("Ödenecek :" + odeme);
     }
     
 }
