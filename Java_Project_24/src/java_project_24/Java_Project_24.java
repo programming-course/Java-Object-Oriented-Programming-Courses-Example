d61cce874200c8091cae7df4222a8c55f2fa74ab
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-01 13:55:37 +0300

    master

--- Java_Project_24/src/java_project_24/Java_Project_24.java
+++ Java_Project_24/src/java_project_24/Java_Project_24.java
@@ -3,19 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package java_project_25;
+package java_project_24;
 
 /**
  *
  * @author ismailtasdelen
  */
-public class Java_Project_25 {
+public class Java_Project_24 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //
+        // Örnek :
+        //
+        String s1 = "123";
+        int x1 = Integer.valueOf(s1);
+        System.out.println(x1-2);
+        //
+        // Örnek :
+        //
+        String s2 = "123";
+        int x2 = Integer.parseInt(s1);
+        System.out.println(x2-2);
     }
     
 }
