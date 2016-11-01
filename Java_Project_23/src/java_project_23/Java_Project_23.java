beb903d650e81ce3b947e6c4b4d4cb81720a8927
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-01 13:52:48 +0300

    master

---
+++ Java_Project_23/src/java_project_23/Java_Project_23.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_23;
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class Java_Project_23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //
+        // Örnek :
+        //
+        byte sayi = 3;
+        int yeni;
+        yeni = (int)sayi;
+        System.out.println(yeni);
+    }
+    
+}
