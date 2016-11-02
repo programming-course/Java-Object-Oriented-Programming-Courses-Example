3309805fa2c60f201b3454171b7da433a0dcd58c
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-02 13:10:39 +0300

    master

---
+++ Java_Project_29/src/java_project_29/Java_Project_29.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_29;
+import java.util.Scanner;
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class Java_Project_29 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //
+        // Örnek :
+        //
+        int tutar;
+        int r_indirim;
+        int p_indirim;
+        int n_indirim;
+        int indirim;
+        int sonuc;
+        Scanner tara = new Scanner(System.in);
+        System.out.println("Alışveriş tutarını giriniz : ");
+        tutar = tara.nextInt();
+        System.out.println("R indirimini giriniz : ");
+        r_indirim = tara.nextInt();
+        System.out.println("P indirimini giriniz : ");
+        p_indirim = tara.nextInt();
+        System.out.println("N indirimini giriniz : ");
+        n_indirim = tara.nextInt();
+        indirim = tutar * (r_indirim / 100) + (n_indirim - tutar) * (p_indirim /100);
+        sonuc = n_indirim - indirim;
+        System.out.println(sounc);
+    }
+    
+}
