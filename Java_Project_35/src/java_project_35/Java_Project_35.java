d01b9ba5fc72e4746b8bd03d32d0638e89fac07a
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-15 10:38:41 +0300

    master

---
+++ Java_Project_35/src/java_project_35/Java_Project_35.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_35;
+
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class Java_Project_35 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int toplam = 0;
+        for(int i = 1; i <= 100; i++)
+        {
+            toplam = toplam + i;
+        }
+        System.out.println(toplam);
+    }
+    
+}
