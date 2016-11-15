b24f5c22099f1c31a261f158f8a58e6051902a4e
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-15 10:33:17 +0300

    master

---
+++ Java_Project_34/src/java_project_34/Java_Project_34.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_34;
+
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class Java_Project_34 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i<=100; i++)
+        {
+            System.out.println(i);
+        }
+        
+    }
+    
+}
