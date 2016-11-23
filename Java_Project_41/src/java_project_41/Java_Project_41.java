43c3b561be1e8f1bf3007e846c1de34e8e0952cb
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-23 12:21:57 +0300

    master

---
+++ Java_Project_41/src/java_project_41/Java_Project_41.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_41;
+
+/**
+ *
+ * @author root
+ */
+public class Java_Project_41 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //
+        // Örnek 1 :
+        //
+        byte [] notlar = {12,34,24,0,89,34,0};
+        System.out.println(notlar[4]);
+        //
+        // Örnek 2 :
+        //
+        notlar[3]=44;
+        System.out.println(notlar[3]);
+    }
+    
+}
---
+++ Java_Project_42/src/java_project_42/Java_Project_42.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_42;
+
+/**
+ *
+ * @author root
+ */
+public class Java_Project_42 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //
+        // Örnek :
+        //
+        byte [] notlar = {12, 34, 24, 0, 89, 34, 0};
+        for (int i = 0; i < 7; i++ )
+        {
+            System.out.println(i + " . değer = " + notlar[i]);
+        }
+    }
+    
+}
