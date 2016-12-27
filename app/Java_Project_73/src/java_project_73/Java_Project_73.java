a2877213182d9844f8d1decae9ab0f6d13650d19
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-12-27 11:30:28 +0300

    master

---
+++ app/Java_Project_73/src/java_project_73/Java_Project_73.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_73;
+import java.util.Scanner;
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class Java_Project_73 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner tara = new Scanner(System.in);
+        dortgen d1 = new dortgen();
+    }
+    
+}
