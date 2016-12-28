24d1512073336691bc6fd5a8a9badcf0986e8e6e
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-12-28 12:23:18 +0300

    master

--- app/Java_Project_75/src/java_project_75/Java_Project_75.java
+++ app/Java_Project_75/src/java_project_75/Java_Project_75.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package java_project_75;
-
+import java.util.Scanner;
 /**
  *
  * @author ismailtasdelen
@@ -14,8 +14,23 @@ public class Java_Project_75 {
     /**
      * @param args the command line arguments
      */
+    public static void aritmekseri(int a, int d, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int sonuc = a+i*d;
+            System.out.println(sonuc);
+        }
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner tara = new Scanner(System.in);
+        System.out.println("A değerini belirleyiniz : ");
+        int a = tara.nextInt();
+        System.out.println("B değerini belirleyiniz : ");
+        int d = tara.nextInt();
+        System.out.println("N değerini belirleyiniz : ");
+        int n = tara.nextInt();
     }
     
 }
