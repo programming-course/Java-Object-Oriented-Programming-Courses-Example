147de3576cdb0fb52d45fac2e189f672a74e9db8
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2017-02-14 12:21:36 +0300

    master

---
+++ app/Java_Project_77/src/java_project_77/Java_Project_77.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_77;
+
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class Java_Project_77 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        dortgen d1 = new dortgen();
+        d1.boy=4;
+    }
+    
+}
---
+++ app/Java_Project_77/src/java_project_77/dortgen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_77;
+
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class dortgen {
+    public double en;
+    public double boy;
+    public double alan_hesapla(){
+    return en * boy;
+    }
+    
+}
