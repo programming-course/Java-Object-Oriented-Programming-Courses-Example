afede85f780089471ea49a22d7fd01b22b0fac07
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2017-02-14 13:36:40 +0300

    master

---
+++ app/Java_Project_78/src/java_project_78/dortgen.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_78;
+
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class dortgen {
+    public double en;
+    private double boy;
+    public static int nesnesay=10;
+    public int NesneSayGoster()
+    {
+        return nesnesay;
+    }
+    public void NesneSayisiArtir()
+    {
+        nesnesay++;
+    }
+}
