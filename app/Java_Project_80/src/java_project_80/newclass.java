a3f3b147bad1ebd87849143c9b61f2a3a8b6cb1f
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2017-02-16 12:58:38 +0300

    master

---
+++ app/Java_Project_80/src/java_project_80/newclass.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_project_80;
+
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class newclass {
+    private String bolumkodu;
+    private String numarasi;
+    private String kayityili;
+    private static int ogrencisayisi=0;
+    public String adsoyad;
+    public String sinifi;
+    public int [][]notlar=new int[8][2];
+    public String []dersler = new String[8];
+    public String get_bolumkodu()
+    {
+        return bolumkodu;
+    }
+    public String get_numarasi()
+    {
+        return numarasi;
+    }
+    public String kayityili()
+    {
+        return kayityili;
+    }
+    public int get_ogrencisayisi()
+    {
+        return ogrencisayisi;
+    }
+    public void set_bolumkodu(String bolumkodu)
+    {
+        this.bolumkodu= bolumkodu;
+    }
+    public void set_numarasi(String numarasi)
+    {
+        this.numarasi= numarasi;
+    }
+    public void set_kayityili(String kayityili)
+    {
+        this.kayityili = kayityili;
+    }
+    public newclass(String adsoyad, String bolumkodu)
+    {
+        this.bolumkodu=bolumkodu;
+        this.adsoyad=adsoyad;
+        ogrencisayisi++;
+    }
+    public newclass(String adsoyad)
+    {
+        this.adsoyad=adsoyad;
+        ogrencisayisi++;
+    }
+    public newclass(String adsoyad, String kayityili, String sinifi)
+    {
+        this.adsoyad=adsoyad;
+        this.sinifi=sinifi;
+        this.kayityili=kayityili;
+        ogrencisayisi++;
+    }
+    
+}
